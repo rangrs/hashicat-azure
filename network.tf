b7c60bcbeed258c843fdd9f06a38c60644b15874
rangrs <89332353+rangrs@users.noreply.github.com>
2023-05-02 15:51:38 +0300

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source  = "app.terraform.io/rangrs/network/azurerm"
+  source  = "app.terraform.io/app/rangrs/registry/modules/private/rangrs/network/azurerm/4.0.0"
   version = "4.0.0"
   resource_group_name = "rangrs"
   # insert required variables here
