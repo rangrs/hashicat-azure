cc4972dc1d2ef2c7a82aae2b95387f8bd7f6c3b3
rangrs <89332353+rangrs@users.noreply.github.com>
2023-05-02 16:00:13 +0300

    Update network.tf

--- network.tf
+++ network.tf
@@ -2,5 +2,6 @@ module "network" {
   source  = "app.terraform.io/rangrs/network/azurerm"
   resource_group_name = "rangrs"
   vnet_location = "eastus"
+  use_for_each = "true"
   # insert required variables here
 }
